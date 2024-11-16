5b2b87053ac0880177b55e2196abaa7f59028ce3
coderepo-deepak <sdpk.ks2@gmail.com>
2024-11-16 11:41:11 +0000

    change 1

--- obj/Debug/net8.0/CICD-Web-App.AssemblyInfo.cs
+++ obj/Debug/net8.0/CICD-Web-App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICD-Web-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d495c54a4d74e2d12274f822a5479f1e50e179")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICD-Web-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICD-Web-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
